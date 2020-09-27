fb8e03116d43958a4004d027b204f59a15bc7a77
Braian Emanuel Lopez <braian.lopez.ze@outlook.com>
2020-09-27 04:26:13 -0300

    Second deploy

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-mongodb+srv://braianlopez:<password>@cluster0.1k17z.mongodb.net/<dbname>?retryWrites=true&w=majority
-mongoose.connect(`mongodb+srv://braianlopez:${process.env.DB_PASS}@cluster0.1k17z.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  `mongodb+srv://braianlopez:${process.env.DB_PASS}@cluster0.1k17z.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 module.exports = mongoose;
