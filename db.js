56fbcc82bc324f0f23b22b4b8f4aa00ca31f89a5
Braian Emanuel Lopez <braian.lopez.ze@outlook.com>
2020-09-27 04:30:00 -0300

    Second deploy

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  `mongodb+srv://braianlopez:${process.env.DB_PASS}@cluster0.1k17z.mongodb.net/${proccess.env.DB_NAME}?retryWrites=true&w=majority`,
+  `mongodb+srv://braianlopez:${process.env.DB_PASS}@cluster0.1k17z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
